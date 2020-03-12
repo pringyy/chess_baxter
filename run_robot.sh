747abec62be1de62810988bbb1586ecff155ce24
Marc Elrick <marcelrick@pop-os.localdomain>
2020-03-12 16:15:22 +0000

    Added spawn_chessboard.py and gazebo2tfframe.py

--- run_robot.sh
+++ run_robot.sh
@@ -1,9 +1,9 @@
 cd ../../
+
 catkin_make
 xfce4-terminal -e "bash devel/setup.bash && roslaunch baxter_gazebo baxter_world.launch";
 xfce4-terminal -e "bash devel/setup.bash && rosrun baxter_tools enable_robot.py -e";
 xfce4-terminal -e "rosrun baxter_interface joint_trajectory_action_server.py";
-xfce4-terminal -e "bash devel/setup.bash && roslaunch baxter_moveit_config baxter_grippers.launch";
+xfce4-terminal -e "bash devel/setup.bash && roslaunch chess_baxter spawn_chessboard.py";
+xfce4-terminal -e "bash devel/setup.bash && roslaunch chess_baxter gazebo2tfframe.py";
 xfce4-terminal -e "bash devel/setup.bash && roslaunch chess_baxter pick_and_plave_moveit.py";
-
-
